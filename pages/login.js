d884f05daf840793061f78d9e55e7a09da31f8bc
patteeraL <120932006+patteeraL@users.noreply.github.com>
2025-03-28 23:10:35 +0700

    Login page and style css created

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import styles from "@/styles/Login.module.css"
+import Head from "next/head";
+export default function Login() {
+  return (
+    <>
+    <Head>
+      <title>Login | Teachamon</title>
+      <meta name="keyword" content=""/>
+    </Head>
+    <div className={styles.container}><h3>The Adventure of</h3><h1>TEACHAMON</h1></div>
+    
+    </>
+  );
+}
